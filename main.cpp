11782063f768e7ba2370437c23e3482069603f2b
Yu Fang <113705821+vf0429@users.noreply.github.com>
2022-11-17 13:17:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,19 +12,19 @@ using namespace std;
 //black -> ● 
 //white -> ○
 
-class board
-{
-public: 
+// class board
+// {
+// public: 
     
-    void arr_init();
-    void block();
-    void init(); //create the board
-private:
-    string arr[15][15]; //use a 2-D array to store data
+//     void arr_init();
+//     void block();
+//     void init(); //create the board
+// private:
+//     string arr[15][15]; //use a 2-D array to store data
 
-};
+// };
 
-void board::init() {
+void print_board {
     cout << "    0   1   2   3   4   5   6   7   8   9   10  11  12  13  14" << endl;
     for (int i = 0; i<15; i++) {
         cout << "  ·---+---+---+---+---+---+---+---+---+---+---+---+---+---+---·" << endl;
@@ -45,13 +45,35 @@ void board::init() {
     cout << "  ·---+---+---+---+---+---+---+---+---+---+---+---+---+---+---·" << endl;
 }
 
-void board::arr_init() { //arr is for storage of use input data
-    for (int i = 0; i<15; i++) {
-        for (int j = 0; j<15; j++) {
-            arr[i][j] = " "; //initially is blank or x(blocked), after input, change to ● (black) or ○ (white)
-        }
+// this function is used to take in the user input and change the values inside arr
+// this function will avoid the case 1) user input is out of range 2)the user input is already occupied
+void player(int &flag,int col, int row, string arr[15][15]){
+  int check = 0;
+  while (check == 0){
+    cout << "Player " << flag%2 +1 << ": ";
+    cin >> col >> row;
+    if (col>=15 || row >=15){
+      cout << "Invalid Input, try again..." << endl;
     }
+    else if (arr[col][row]=="●" || arr[col][row]=="○" ){
+      cout << "Invalid Input, try again..." << endl;
+    }
+    else
+      check++;
+  }
+
+  if (flag%2==0){
+    arr[col][row] = "●";
+    flag++;
+  }
+
+  else{
+    arr[col][row] = "○";
+    flag++;
+  }
+  cout << endl;
 }
+
 
 void board::block() { //initialize board with blocks stopping user input
     int number = 20; //number of blocks, can be modified by user later
@@ -78,9 +100,20 @@ void board::block() { //initialize board with blocks stopping user input
 
 int main() {
     
-    board gobang_board;
-    gobang_board.arr_init();
-    gobang_board.block();
-    gobang_board.init();
+//     board gobang_board;
+//     gobang_board.arr_init();
+//     gobang_board.block();
+//     gobang_board.init();
 
+    for (int i = 0; i<15; i++) {
+        for (int j = 0; j<15; j++) {
+            arr[i][j] = " "; //initially is blank or x(blocked), after input, change to ● (black) or ○ (white)
+        }
+    }
+    while (true){ // define a function which will return true if 5 in a line
+    player(flag,col,row,arr);
+    if (flag>=1){
+      system("clear");
+    }
+    print_board();
 }
