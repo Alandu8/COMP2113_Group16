21c1666e9d283ea3e518a5d68b4a2a1c69122c0a
Yu Fang <113705821+vf0429@users.noreply.github.com>
2022-11-17 13:24:08 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,8 +23,11 @@ using namespace std;
 //     string arr[15][15]; //use a 2-D array to store data
 
 // };
+int flag =0;
+int col,row;
+string arr[15][15];
 
-void print_board {
+void print_board(){
     cout << "    0   1   2   3   4   5   6   7   8   9   10  11  12  13  14" << endl;
     for (int i = 0; i<15; i++) {
         cout << "  ·---+---+---+---+---+---+---+---+---+---+---+---+---+---+---·" << endl;
@@ -47,6 +50,7 @@ void print_board {
 
 // this function is used to take in the user input and change the values inside arr
 // this function will avoid the case 1) user input is out of range 2)the user input is already occupied
+
 void player(int &flag,int col, int row, string arr[15][15]){
   int check = 0;
   while (check == 0){
@@ -75,28 +79,28 @@ void player(int &flag,int col, int row, string arr[15][15]){
 }
 
 
-void board::block() { //initialize board with blocks stopping user input
-    int number = 20; //number of blocks, can be modified by user later
-    srand(time(0)); //problem with random seed, random_shuffle not random
-    vector<string> board_item;
-    for (int i = 0; i<15; i++) {
-        for (int j = 0; j<15; j++) {
-            board_item.push_back(arr[i][j]);
-        }
-    }
-    vector<string>::iterator itr = board_item.begin();
-    for (int i = 0; i< number; i++) {
-        *itr = "x";
-        itr++;
-    }
-    random_shuffle(board_item.begin(), board_item.end());
+// void board::block() { //initialize board with blocks stopping user input
+//     int number = 20; //number of blocks, can be modified by user later
+//     srand(time(0)); //problem with random seed, random_shuffle not random
+//     vector<string> board_item;
+//     for (int i = 0; i<15; i++) {
+//         for (int j = 0; j<15; j++) {
+//             board_item.push_back(arr[i][j]);
+//         }
+//     }
+//     vector<string>::iterator itr = board_item.begin();
+//     for (int i = 0; i< number; i++) {
+//         *itr = "x";
+//         itr++;
+//     }
+//     random_shuffle(board_item.begin(), board_item.end());
     
-    for (int i = 0; i<15; i++) {
-        for (int j = 0; j<15; j++) {
-            arr[i][j] = board_item[i*15+j];
-        }
-    }
-}
+//     for (int i = 0; i<15; i++) {
+//         for (int j = 0; j<15; j++) {
+//             arr[i][j] = board_item[i*15+j];
+//         }
+//     }
+// }
 
 int main() {
     
@@ -111,9 +115,10 @@ int main() {
         }
     }
     while (true){ // define a function which will return true if 5 in a line
-    player(flag,col,row,arr);
-    if (flag>=1){
-      system("clear");
+        player(flag,col,row,arr);
+        if (flag>=1){
+          system("clear");
+        }
+        print_board();
     }
-    print_board();
 }
